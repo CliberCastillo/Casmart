8a138e350800b9979c92c4939f885b7f377a2712
Cliber <clibercg@gmail.com>
2019-11-09 12:49:19 -0500

    Agregando archivos estaticos

--- Buses/Controllers/PaginaPrincipalController.cs
+++ Buses/Controllers/PaginaPrincipalController.cs
@@ -8,32 +8,12 @@ using Buses.Models;
 
 namespace Buses.Controllers
 {
-    public class HomeController : Controller
+    public class PaginaPrincipalController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
