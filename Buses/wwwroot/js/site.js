8a1cfcd04e7d2b75a809f814b4f923f7e5dcd587
cliber <clibercg@gmail.com>
2019-11-12 12:34:16 -0500

    agregando logos

--- Buses/wwwroot/js/site.js
+++ Buses/wwwroot/js/site.js
@@ -2,3 +2,167 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//function zoom() {
+//      document.getElementById('imagenTuristico').style.width = '90px';
+//}
+//function desZoom() {
+//    document.getElementById('imagenTuristico').style.width = '80px';
+//}
+
+$(document).ready(function () {
+    $("#imagenTuristico").mouseover(function () {
+        $(this).css("width", "90");
+    });
+
+    $("#imagenTuristico").mouseout(function () {
+        $(this).css("width", "80");
+    });
+});
+//////////////////////////////
+$(document).ready(function () {
+    $("#visa").mouseover(function () {
+        $(this).css("width", "80");
+    });
+
+    $("#visa").mouseout(function () {
+        $(this).css("width", "70");
+    });
+});
+
+$(document).ready(function () {
+    $("#american").mouseover(function () {
+        $(this).css("width", "80");
+    });
+
+    $("#american").mouseout(function () {
+        $(this).css("width", "70");
+    });
+});
+
+
+$(document).ready(function () {
+    $("#mastercard").mouseover(function () {
+        $(this).css("width", "80");
+    });
+
+    $("#mastercard").mouseout(function () {
+        $(this).css("width", "70");
+    });
+});
+
+
+$(document).ready(function () {
+    $("#pagoEfectivo").mouseover(function () {
+        $(this).css("width", "144");
+    });
+
+    $("#pagoEfectivo").mouseout(function () {
+        $(this).css("width", "126");
+    });
+});
+
+
+$(document).ready(function () {
+    $("#diners").mouseover(function () {
+        $(this).css("width", "80");
+    });
+
+    $("#diners").mouseout(function () {
+        $(this).css("width", "70");
+    });
+});
+
+
+$(document).ready(function () {
+    $("#paypal").mouseover(function () {
+        $(this).css("width", "80");
+    });
+
+    $("#paypal").mouseout(function () {
+        $(this).css("width", "70");
+    });
+});
+
+
+//$(document).ready(function () {
+//    $("#imagen1").mouseover(function () {
+//        $(this).css("width", "300");
+//    });
+
+//    $("#imagen1").mouseout(function () {
+//        $(this).css();
+//    });
+//});
+
+//$(document).ready(function () {
+//    $("#imagen2").mouseover(function () {
+//        $(this).css("width", "100");
+//    });
+
+//    $("#imagen2").mouseout(function () {
+//        $(this).css("width", "80");
+//    });
+//});
+
+//$(document).ready(function () {
+//    $("#imagen3").mouseover(function () {
+//        $(this).css("width", "100");
+//    });
+
+//    $("#imagen3").mouseout(function () {
+//        $(this).css("width", "80");
+//    });
+//});
+
+//$(document).ready(function () {
+//    $("#imagen4").mouseover(function () {
+//        $(this).css("width", "100");
+//    });
+
+//    $("#imagen4").mouseout(function () {
+//        $(this).css("width", "80");
+//    });
+//});
+$(document).ready(function () {
+    $("#linkedin").mouseover(function () {
+        $(this).css("width", "40");
+    });
+
+    $("#linkedin").mouseout(function () {
+        $(this).css("width", "30");
+    });
+});
+
+
+$(document).ready(function () {
+    $("#youtube").mouseover(function () {
+        $(this).css("width", "40");
+    });
+
+    $("#youtube").mouseout(function () {
+        $(this).css("width", "30");
+    });
+});
+
+
+$(document).ready(function () {
+    $("#whatsapp").mouseover(function () {
+        $(this).css("width", "40");
+    });
+
+    $("#whatsapp").mouseout(function () {
+        $(this).css("width", "30");
+    });
+});
+
+
+$(document).ready(function () {
+    $("#instagram").mouseover(function () {
+        $(this).css("width", "40");
+    });
+
+    $("#instagram").mouseout(function () {
+        $(this).css("width", "30");
+    });
+});
