81da59387b2eb65f4d74c92cfdfa19b715d1eafc
Cliber Castillo <clibercg@gmail.com>
2019-11-21 10:29:51 -0500

    page hora

---
+++ Buses/Controllers/PasajeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Buses.Controllers
+{
+    public class PasajeController : Controller
+    {
+        public IActionResult Hora()
+        {
+            return View();
+        }
+    }
+}
