5a959404cfdfd7af632426047b5d08df5be8ec45
Cliber Castillo <clibercg@gmail.com>
2019-12-01 17:45:07 -0500

    commit

--- Buses/Controllers/PaginaPrincipalController.cs
+++ Buses/Controllers/PaginaPrincipalController.cs
@@ -21,13 +21,13 @@ namespace Buses.Controllers
             var viajes = _mantenimientoViaje.ObtenerListadoAgencia();
             return View(viajes);
         }
-        public IActionResult Login(string usuario, string contraseña)
+        public JsonResult Login(string correo)
         {
-            if (_mantenimientoViaje.Login(usuario,contraseña))
-            {
-                ViewBag.message = "Bienvenido";
-            }
-            return RedirectToAction("Index");
+            //if (_mantenimientoViaje.Login(usuario,contraseña))
+            //{
+            //    ViewBag.message = "Bienvenido";
+            //}
+            return Json("");
         }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ Buses/wwwroot/js/PaginaPrincipal/paginaPrincipal.js
@@ -0,0 +1,19 @@
+﻿
+$("#botonIniciarSesion").click(function (e) {
+    var txtCorreo = $("#txtCorreo").val();
+    alert(txtCorreo);
+    $.ajax({
+        url: '/PaginaPrincipal/Login',
+        type: 'GET',
+        dataType: 'Json',
+        contentType: 'application/json; charset=utf-8',
+        data: { correo: txtCorreo },
+        success: function (data) {
+
+        },
+        error: function (jqXhr, textStatus, errorThrown) {
+            console.log(errorThrown);
+        }
+    });
+
+});
