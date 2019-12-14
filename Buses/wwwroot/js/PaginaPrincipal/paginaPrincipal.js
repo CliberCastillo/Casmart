42ba851d883b4968ef64f704f350d9c9f3b9f4ca
Cliber <clibercg@gmail.com>
2019-12-14 13:46:54 -0500

    corrigiendo error de ajax

--- Buses/wwwroot/js/PaginaPrincipal/paginaPrincipal.js
+++ Buses/wwwroot/js/PaginaPrincipal/paginaPrincipal.js
@@ -1,19 +1,78 @@
-﻿
-$("#botonIniciarSesion").click(function (e) {
-    var txtCorreo = $("#txtCorreo").val();
-    alert(txtCorreo);
+﻿function mostrarPassword() {
+    var cambio = document.getElementById("txtPassword");
+    var cambio2 = document.getElementById("txtPassword2");
+    if (cambio.type === "password" & cambio.type === "password") {
+        cambio.type = "text";
+        cambio2.type = "text";
+        $('.icon').removeClass('fa fa-eye-slash').addClass('fa fa-eye');
+    } else {
+        cambio.type = "password";
+        cambio2.type = "password";
+        $('.icon').removeClass('fa fa-eye').addClass('fa fa-eye-slash');
+    }
+}
+
+//$("#botonIniciarSesion").click(function (e) {
+//    var txtCorreo = $("#txtCorreo").val();
+//    $.ajax({
+//        url: '/Account/Login',
+//        type: 'GET',
+//        dataType: 'Json',
+//        contentType: 'application/json; charset=utf-8',
+//        data: { correo: txtCorreo },
+//        success: function (data) {
+
+//        },
+//        error: function (jqXhr, textStatus, errorThrown) {
+//            console.log(errorThrown);
+//        }
+//    });
+//});
+
+$("#btnRegistrarme").click(function (e) {
+    var nombre = $("#Nombre").val();
+    var email = $("#Email").val();
+    var contraseña = $("#txtPassword").val();
+    var contraseña1 = $("#txtPassword2").val();
+    $("#Nombre").val('');
+    $("#Email").val('');
+    $("#txtPassword").val('');
+    $("#txtPassword2").val('');
+
     $.ajax({
-        url: '/PaginaPrincipal/Login',
+        url: '/Account/CreateUser',
         type: 'GET',
         dataType: 'Json',
+        async: false,
         contentType: 'application/json; charset=utf-8',
-        data: { correo: txtCorreo },
-        success: function (data) {
-
+        data: { Nombre: nombre, Email: email, Contraseña: contraseña },
+        success: function (data, textStatus, jQxhr) {
+            if (data) {
+                swal({
+                    title: "Registrado!",
+                    text: "Se registró correctamente :D!",
+                    icon: "success",
+                    button: "Aceptar"
+                });
+            }
+            else {
+                swal({
+                    title: "No Registrado!",
+                    text: "No se registró, por favor ingresar los datos requeridos :C!",
+                    icon: "error",
+                    button: "Aceptar"
+                });
+            }
+            $('#btnRegistrarme').modal('hide');
         },
         error: function (jqXhr, textStatus, errorThrown) {
-            console.log(errorThrown);
+            swal({
+                title: "Error!",
+                text: "Por favor completar todo los campos requeridos",
+                icon: "error",
+                button: "Aceptar"
+            });
         }
     });
-
 });
+
--- Buses/wwwroot/js/site.js
+++ Buses/wwwroot/js/site.js
@@ -1,15 +1,5 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-
-//function zoom() {
-//      document.getElementById('imagenTuristico').style.width = '90px';
-//}
-//function desZoom() {
-//    document.getElementById('imagenTuristico').style.width = '80px';
-//}
-
 $(document).ready(function () {
     $("#imagenTuristico").mouseover(function () {
         $(this).css("width", "90");
@@ -95,67 +85,4 @@ $(document).ready(function () {
         $(this).css("width", "30");
     });
 
-    $("#btnRegistrar").click(function (e) {
-        var nombre = $("#Nombre").val();
-        var email = $("#Email").val();
-        var contraseña = $("#txtPassword").val();
-        var contraseña1 = $("#txtPassword2").val();
-        nombre.val('');
-        email.val('');
-        contraseña.val('');
-        contraseña1.val('');
-
-        $.ajax({
-            url: '/Account/CreateUser',
-            type: 'GET',
-            dataType: 'Json',
-            async: false,
-            contentType: 'application/json; charset=utf-8',
-            data: { Nombre: nombre, Email: email, Contraseña: contraseña },
-            success: function (data, textStatus, jQxhr) {
-                if (data) {
-                    swal({
-                        title: "Registrado!",
-                        text: "Se registró correctamente :D!",
-                        icon: "success",
-                        button: "Aceptar"
-                    });
-                }
-                else {
-                    swal({
-                        title: "No Registrado!",
-                        text: "No se registró, por favor ingresar los datos requeridos :C!",
-                        icon: "error",
-                        button: "Aceptar"
-
-                    });
-                }
-                $('#ModalRegistrarme').modal('hide');
-            },
-            error: function (jqXhr, textStatus, errorThrown) {
-                swal({
-                    title: "Error!",
-                    text: "Por favor completar todo los campos requeridos",
-                    icon: "error",
-                    button: "Aceptar"
-                });
-            }
-        });
-
-    });
-
 });
-
-function mostrarPassword() {
-    var cambio = document.getElementById("txtPassword");
-    var cambio2 = document.getElementById("txtPassword2");
-    if (cambio.type === "password" & cambio.type === "password") {
-        cambio.type = "text";
-        cambio2.type = "text";
-        $('.icon').removeClass('fa fa-eye-slash').addClass('fa fa-eye');
-    } else {
-        cambio.type = "password";
-        cambio2.type = "password";
-        $('.icon').removeClass('fa fa-eye').addClass('fa fa-eye-slash');
-    }
-}
