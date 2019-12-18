9a26af6a571759f65aede08e1054cff3c97c58c7
Cliber Castillo <clibercg@gmail.com>
2019-12-18 17:08:40 -0500

    update promociones

--- Buses/wwwroot/js/site.js
+++ Buses/wwwroot/js/site.js
@@ -1,88 +1,87 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
-$(document).ready(function () {
-    $("#imagenTuristico").mouseover(function () {
+$(document).ready(function() {
+    $("#imagenTuristico").mouseover(function() {
         $(this).css("width", "90");
     });
 
-    $("#imagenTuristico").mouseout(function () {
+    $("#imagenTuristico").mouseout(function() {
         $(this).css("width", "80");
     });
 
-    $("#visa").mouseover(function () {
+    $("#visa").mouseover(function() {
         $(this).css("width", "80");
     });
 
-    $("#visa").mouseout(function () {
+    $("#visa").mouseout(function() {
         $(this).css("width", "70");
     });
 
-    $("#american").mouseover(function () {
+    $("#american").mouseover(function() {
         $(this).css("width", "80");
     });
 
-    $("#american").mouseout(function () {
+    $("#american").mouseout(function() {
         $(this).css("width", "70");
     });
 
-    $("#mastercard").mouseover(function () {
+    $("#mastercard").mouseover(function() {
         $(this).css("width", "80");
     });
 
-    $("#mastercard").mouseout(function () {
+    $("#mastercard").mouseout(function() {
         $(this).css("width", "70");
     });
 
-    $("#pagoEfectivo").mouseover(function () {
+    $("#pagoEfectivo").mouseover(function() {
         $(this).css("width", "144");
     });
 
-    $("#pagoEfectivo").mouseout(function () {
+    $("#pagoEfectivo").mouseout(function() {
         $(this).css("width", "126");
     });
 
-    $("#diners").mouseover(function () {
+    $("#diners").mouseover(function() {
         $(this).css("width", "80");
     });
 
-    $("#diners").mouseout(function () {
+    $("#diners").mouseout(function() {
         $(this).css("width", "70");
     });
 
-    $("#paypal").mouseover(function () {
+    $("#paypal").mouseover(function() {
         $(this).css("width", "80");
     });
 
-    $("#paypal").mouseout(function () {
+    $("#paypal").mouseout(function() {
         $(this).css("width", "70");
     });
 
-    $("#linkedin").mouseover(function () {
+    $("#linkedin").mouseover(function() {
         $(this).css("width", "40");
     });
 
-    $("#linkedin").mouseout(function () {
+    $("#linkedin").mouseout(function() {
         $(this).css("width", "30");
     });
 
-    $("#youtube").mouseover(function () {
+    $("#youtube").mouseover(function() {
         $(this).css("width", "40");
     });
 
-    $("#youtube").mouseout(function () {
+    $("#youtube").mouseout(function() {
         $(this).css("width", "30");
     });
 
-    $("#whatsapp").mouseover(function () {
+    $("#whatsapp").mouseover(function() {
         $(this).css("width", "40");
     });
 
-    $("#instagram").mouseover(function () {
+    $("#instagram").mouseover(function() {
         $(this).css("width", "40");
     });
 
-    $("#instagram, #whatsapp, #youtube").mouseout(function () {
+    $("#instagram, #whatsapp, #youtube").mouseout(function() {
         $(this).css("width", "30");
     });
-
 });
