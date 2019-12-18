3bcf6c8f905fdd13d04f3be5a1edc2c9a7bc5e83
Cliber Castillo <clibercg@gmail.com>
2019-12-18 15:39:52 -0500

    promociones

--- Buses/Migrations/20191218200248_DescripcionIntinerarioViaje.Designer.cs
+++ Buses/Migrations/20191218200248_DescripcionIntinerarioViaje.Designer.cs
@@ -4,14 +4,16 @@ using Buses.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Buses.Migrations
 {
     [DbContext(typeof(AgenciaBusesContext))]
-    partial class AgenciaBusesContextModelSnapshot : ModelSnapshot
+    [Migration("20191218200248_DescripcionIntinerarioViaje")]
+    partial class DescripcionIntinerarioViaje
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Buses/Migrations/20191218200248_DescripcionIntinerarioViaje.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Buses.Migrations
+{
+    public partial class DescripcionIntinerarioViaje : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DescripcionViaje",
+                table: "ItinerarioViaje",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DescripcionViaje",
+                table: "ItinerarioViaje");
+        }
+    }
+}
